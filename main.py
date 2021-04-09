ce5a68ce2997a4605aceca997c3cd595fd4186a5
ninglang <yzmninglang@gmial.com>
2021-04-09 14:24:30 +0800

    start

--- main.py
+++ main.py
@@ -2,9 +2,9 @@ import msvcrt
 import sys
 import os
 import csv
-import data
-import time
-import category
+# import data
+# import time
+# import category
 def home():
     os.system('cls')
     select=[" ","1-显示时间记录","2-添加时间记录","3-删除时间记录","4-修改时间记录","5-分析时间记录","6-退出系统"]
@@ -39,151 +39,6 @@ def decide(choice):
     else :
         print("see you!")
         sys.exit()
-
-
-
-def TimeRecordH():
-    os.system('cls')
-    choice='p'
-    i=0;j=10
-    while choice!='q':
-        hang=data.Datashow((i,j))
-        # if i==0:
-        #     hang=data.Datashow((i,(i+1)*10))
-        # else:
-        #     hang=data.Datashow(((i-1)*10,i*10))
-        # i+=1
-        i,j=j,j+10
-        print(i)
-        print("Please input what do you want to do?\n[n-next page;s-search;p-picture;q-quit]:")
-        choice=msvcrt.getch().decode()  
-        print(choice)
-        if choice=='n':
-            # os.system('cls')
-            try:
-                # print(i)
-                # time.sleep(5)
-                if hang-j>10  :
-                    # print(i)
-                    # if i==1:
-                    #     data.Datashow((i,j))
-                    # else:
-                    #     data.Datashow(((i-1)*10,i*10))
-                    data.Datashow((i,j))
-                    print(i)
-                    # i+=1
-                    i,j=j,j+10
-                    print("Please input what do you want to do?\n[n-next page;s-search;p-picture;q-quit]:")
-                    choice=msvcrt.getch()
-                else:
-                    pass
-                    i=j
-                    data.Datashow((i,hang))
-                    print(i)
-                    i,j=0,10
-                    print("Please input what do you want to do?\n[n-next page;s-search;p-picture;q-quit]:")
-                    choice=msvcrt.getch().decode()
-            except:
-                print("sorry,data is over")
-                # time.sleep()
-                continue
-        elif choice=='s':
-            print("\rPlease enter the type you want to search?\n[1.time,2-category,3-content]:")
-            choice=eval(msvcrt.getch())
-            if choice==1:
-                print("\rInput by form [M-D or search by Y]:")
-                date=input()
-                os.system('cls')
-                data.searchbytime(date) 
-                choice=eval(msvcrt.getch())
-
-                                            #give a delete function
-                # if '-' in date :
-                #     data.searchbytime()
-                #     # time.sleep(10)
-                # else:
-                #     date.searchbyyear()
-                    # time.sleep(10)
-
-            elif choice==2:
-                #search by category
-                print("\rPlease input Category:")
-                Category=input()
-                os.system('cls')
-                data.SearchByCate(Category) 
-                choice=eval(msvcrt.getch())
-                if choice=='q':
-                    data.Datashow((1,10))
-
-            elif choice==3:
-                print("\rPlease input Content")
-                content=input()
-                os.system('cls')
-                data.SearchByContent(content)
-                choice=str(msvcrt.getch())
-                if choice=='q':
-                    data.Datashow((1,10))
-        
-
-            # DeleteTime()
-            # pass
-        elif choice=='p':
-            AnalysisUi()
-
-        else :
-            print("see you!")
-            sys.exit()
-
-
-def AddTimeH():
-    data.Datashow((0,10))
-    print("Please input what type data you want to enter\n[1.time,2.clock,3.date and clock]:")
-    choice=msvcrt.getch()
-    if choice==1:
-        print("Please input how much time you have cost:")
-        time=eval(input())
-        print("Please input the content of activity:")
-        content=input()
-        if judge(content):
-            addbytime(content,time,data.GetCateBycontent(content))
-        else:
-            print("Please input the Category:")
-            category=input()
-
-
-    elif choice==2:
-        print("Please input how much time you have cost:")
-        time=eval(input())
-        print("Please input the content of activity:")
-        if judge(content):
-            addbyclock(content,time,data.GetCateBycontent(content))
-        else:
-            print("Please input the Category:")
-            category=input()
-            addbyclock(content,time,data.GetCateBycontent(content))
-
-            
-
-    elif choice==3:
-        addbydate()
-# def getinputdata():
-#     data=input("Pla")
-
-
-def judge(content):
-
-
-# def addbydate():
-#     s
-
-
-
-def DeleteTime():
-    pass
-
-
-def ChangeTime():
-    pass
 
 
 def AnalysisUi():
@@ -221,3 +76,147 @@ def AnalysisUi():
 def main(): 
     home()
 main()
+
+# def TimeRecordH():
+#     os.system('cls')
+#     choice='p'
+#     i=0;j=10
+#     while choice!='q':
+#         hang=data.Datashow((i,j))
+#         # if i==0:
+#         #     hang=data.Datashow((i,(i+1)*10))
+#         # else:
+#         #     hang=data.Datashow(((i-1)*10,i*10))
+#         # i+=1
+#         i,j=j,j+10
+#         print(i)
+#         print("Please input what do you want to do?\n[n-next page;s-search;p-picture;q-quit]:")
+#         choice=msvcrt.getch().decode()  
+#         print(choice)
+#         if choice=='n':
+#             # os.system('cls')
+#             try:
+#                 # print(i)
+#                 # time.sleep(5)
+#                 if hang-j>10  :
+#                     # print(i)
+#                     # if i==1:
+#                     #     data.Datashow((i,j))
+#                     # else:
+#                     #     data.Datashow(((i-1)*10,i*10))
+#                     data.Datashow((i,j))
+#                     print(i)
+#                     # i+=1
+#                     i,j=j,j+10
+#                     print("Please input what do you want to do?\n[n-next page;s-search;p-picture;q-quit]:")
+#                     choice=msvcrt.getch()
+#                 else:
+#                     pass
+#                     i=j
+#                     data.Datashow((i,hang))
+#                     print(i)
+#                     i,j=0,10
+#                     print("Please input what do you want to do?\n[n-next page;s-search;p-picture;q-quit]:")
+#                     choice=msvcrt.getch().decode()
+#             except:
+#                 print("sorry,data is over")
+#                 # time.sleep()
+#                 continue
+#         elif choice=='s':
+#             print("\rPlease enter the type you want to search?\n[1.time,2-category,3-content]:")
+#             choice=eval(msvcrt.getch())
+#             if choice==1:
+#                 print("\rInput by form [M-D or search by Y]:")
+#                 date=input()
+#                 os.system('cls')
+#                 data.searchbytime(date) 
+#                 choice=eval(msvcrt.getch())
+
+#                                             #give a delete function
+#                 # if '-' in date :
+#                 #     data.searchbytime()
+#                 #     # time.sleep(10)
+#                 # else:
+#                 #     date.searchbyyear()
+#                     # time.sleep(10)
+
+#             elif choice==2:
+#                 #search by category
+#                 print("\rPlease input Category:")
+#                 Category=input()
+#                 os.system('cls')
+#                 data.SearchByCate(Category) 
+#                 choice=eval(msvcrt.getch())
+#                 if choice=='q':
+#                     data.Datashow((1,10))
+
+#             elif choice==3:
+#                 print("\rPlease input Content")
+#                 content=input()
+#                 os.system('cls')
+#                 data.SearchByContent(content)
+#                 choice=str(msvcrt.getch())
+#                 if choice=='q':
+#                     data.Datashow((1,10))
+        
+
+#             # DeleteTime()
+#             # pass
+#         elif choice=='p':
+#             AnalysisUi()
+
+#         else :
+#             print("see you!")
+#             sys.exit()
+
+
+# def AddTimeH():
+#     data.Datashow((0,10))
+#     print("Please input what type data you want to enter\n[1.time,2.clock,3.date and clock]:")
+#     choice=msvcrt.getch()
+#     if choice==1:
+#         print("Please input how much time you have cost:")
+#         time=eval(input())
+#         print("Please input the content of activity:")
+#         content=input()
+#         if judge(content):
+#             addbytime(,content,time,data.GetCateBycontent(content))
+#         else:
+#             print("Please input the Category:")
+#             category=input()
+
+
+#     elif choice==2:
+#         print("Please input how much time you have cost:")
+#         time=eval(input())
+#         print("Please input the content of activity:")
+#         if judge(content):
+#             addbyclock(content,time,data.GetCateBycontent(content))
+#         else:
+#             print("Please input the Category:")
+#             category=input()
+#             addbyclock(content,time,data.GetCateBycontent(content))
+
+            
+
+#     elif choice==3:
+#         addbydate()
+# # def getinputdata():
+# #     data=input("Pla")
+
+
+# def judge(content):
+
+#     pass
+# # def addbydate():
+# #     s
+
+
+
+# def DeleteTime():
+#     pass
+
+
+# def ChangeTime():
+#     pass
+

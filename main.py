0c9234108911b36791280b430652d5b377fb80ad
ninglang <yzmninglang>
2021-04-17 15:20:17 +0800

    weather is finished

--- main.py
+++ main.py
@@ -7,6 +7,7 @@ import datetime
 import numpy as np
 import matplotlib.pyplot as plt
 import matplotlib
+import re
 import requests
 import json
 
@@ -41,8 +42,34 @@ def decidedate(date_string):
                 return False
     except:
         return False
-
+weatheris=1
 def home():
+
+    def getweather():
+        head={'User-Agent': "Mozilla/5.0 (Linux; U; Android 10; en; Mi MIX 2S Build/QQ3A.200805.001) AppleWebKit/537.36 (KHTML, like Gecko) Version/4.0 Chrome/57.0.2987.108 UCBrowser/12.10.0.1163 UCTurbo/1.10.3.900 Mobile Safari/537.36"  }
+
+        url="http://mip.chinaz.com/"
+
+        text=requests.get(url=url,headers=head).content.decode("utf-8")
+        location=re.findall('中国(.*?)[电移联][信动通]',text,re.DOTALL)[0]
+
+        provence="河北省、山西省、辽宁省、吉林省、黑龙江省、江苏省、浙江省、安徽省、福建省、江西省、山东省、台湾省、河南省、湖北省、湖南省、广东省、海南省、四川省、贵州省、云南省、陕西省、甘肃省、青海省"
+        provences=provence.replace("省","").split('、')
+        for i in provences:
+            if i in location:
+                locations=location.strip(i)
+                # print(locations)
+
+        url="http://toy1.weather.com.cn/search?cityname={}&callback=success_jsonpCallback&_=1618638366530".format(locations)
+        text=requests.get(url=url,headers=head).content.decode("utf-8")
+        text=re.findall('"ref":"([0-9]*?)~.*?~'.format(locations),text,re.DOTALL)[0]
+        # print(text)
+        getweatherurl="http://d1.weather.com.cn/dingzhi/{}.html".format(text)
+        content=requests.get(url=getweatherurl,headers=head).content.decode("utf-8")
+        content=re.findall("=({.*?}.*?})",content,re.DOTALL)[0]
+        weather=json.loads(content)
+        return weather["weatherinfo"]
+
 
     def decide(choice):
         if choice==1:
@@ -62,6 +89,24 @@ def home():
             sys.exit()
 
     os.system('cls')
+    try:
+        global weatheris
+        if weatheris==1:
+            weather=getweather()
+            f2=open("weather.json","w")
+            f2.write(json.dumps(weather))
+            f2.close()
+            weatheris=0
+        else:
+            f2=open("weather.json","r")
+            content=f2.read()
+            weather=json.loads(content)
+        print("             {:^24s}".format(weather['cityname']))
+        print("            温度：{0}-{1} 天气：{2}".format(weather["temp"],weather["tempn"],weather["weather"]))
+        print("                   风向：{0} \n                风速：{1}".format(weather['wd'],weather["ws"]))
+    except:
+        pass 
+    # print("{}温度:{}气压：{}")
     select=[" ","1-显示时间记录","2-添加时间记录","3-删除时间记录","4-修改时间记录","5-分析时间记录","6-退出系统"]
     print()
     for i in range(0,17):
